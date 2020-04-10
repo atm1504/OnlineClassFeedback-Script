92dd42254005105f6a66a42f6afb3e49ccf64754
Amartya Mondal <hayyoulistentome@gmail.com>
2020-04-10 19:02:21 +0530

    feat: Remove personal data

--- fill-form.py
+++ fill-form.py
@@ -1,11 +1,11 @@
 import requests
 url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSd5NX1jRwPFu1h8ZQz2UAwYA__ihYr0XcwmPafbJjztuuZb6g/formResponse"
 data = {
-    "entry.1564564753": "Amartya Mondal", #Your name
-    "entry.45339518": "1801ME07", #Your roll number
-    "entry.1286705147": "8967570983", #Your phone number
-    "entry.1381581746": "1801me07@iitp.ac.in", #Your email
-    "entry.618759938": "HS202 - Introductory Macroeconomics", #Course name
+    "entry.1564564753": "Your name", #Your name
+    "entry.45339518": "Your roll number", #Your roll number
+    "entry.1286705147": "your phone number", #Your phone number
+    "entry.1381581746": "your institute email id", #Your email
+    "entry.618759938": "Subject name. U can get this from the from", #Course name
     "entry.365693181": "100%",  #Q.1. Compared to in-class, satisfaction with respect to online content delivery is:
     "entry.2105058604": "100%",  #Q.2. Compared to in-class, satisfaction with respect to ease of understanding is :
     "entry.612816140": "100%",  #Q3 Q.3. Compared to in-class, satisfaction with respect to the speed at which topics are covered is: *
